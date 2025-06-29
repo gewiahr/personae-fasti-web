4a961386096e8cee6f63b6e84d5f8a28388c7cb7
gewiahr <gewiahr@gmail.com>
2025-06-30 00:56:17 +0300

    add: settings "start new session" button functionality added

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -7,6 +7,7 @@ import { api } from '../utils/api';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { useNavigate } from 'react-router-dom';
 import { useNotifications } from '../context/NotificationContext';
+import FoldableCategory from '../components/FoldableCategory';
 
 const SettingsPage = () => {
   const navigate = useNavigate();
@@ -37,23 +38,53 @@ const SettingsPage = () => {
     }  
   };
 
+  const handleNewSession = async () => { 
+    const { error, status } = await api.post<GameInfo>("/game/session/new", accessKey, null);
+    if (error) {
+      addNotification(error.message, 'error');
+      return;
+    } 
+
+    if (status === 201) {
+      addNotification('Началась новая сессия', 'success');
+    }   
+  }
+
   return (
     <div className='max-w-4xl mx-auto p-4'>
       <div className='flex flex-col gap-y-6'>
+
         <h2 className='text-xl'>{player.username}</h2>
+
         <div>
-          {currentGame && playerGames && 
-            <SelectInput 
-              key={playerGames.length} 
-              options={playerGames?.
-                filter((game) => game.id != currentGame.id).
-                map((game) => { return { key: game.id, value: game.title } }) || []} 
-              label='Текущая игра' 
-              value={currentGame?.title} 
-              entityEdit={{ handleFieldChange: handleChangeCurrentGame }} 
-            />
+          {currentGame && 
+            <>
+              {playerGames.length > 1 ? 
+              <SelectInput 
+                key={playerGames.length} 
+                options={playerGames?.
+                  filter((game) => game.id != currentGame.id).
+                  map((game) => { return { key: game.id, value: game.title } }) || []} 
+                label='Текущая игра' 
+                value={currentGame.title} 
+                entityEdit={{ handleFieldChange: handleChangeCurrentGame }} 
+              /> : 
+              <>
+                <p className='text-sm'>Текущая игра:</p>
+                <h2 className='text-xl'>{currentGame.title}</h2>
+              </>}
+            </>
           }
         </div>
+
+        <FoldableCategory key="sessions_settings" title='Сессии' children={
+          <button
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white mt-2 py-2 px-4 rounded"
+            onClick={handleNewSession}
+          >
+            {"Начать новую сессию"}
+          </button>
+        }/>
       </div>
     </div>
   )
