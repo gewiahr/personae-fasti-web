012b814907b6106ec74d202c1d765bc44dcd7793
gewi@swift <gewiahr@gmail.com>
2026-01-24 14:25:24 +0700

    refactor: react update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(), tailwindcss()],
-  publicDir: 'public'
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+    mkcert(),
+  ],
 })
