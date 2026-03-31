1d868be2cbf11e3b6915d7011e1d8838505052f9
Tomasz <gewiahr@gmail.com>
2026-03-31 18:19:36 +0700

    refactor: Header component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,54 @@
+import { miniApp } from "@tma.js/sdk-react";
+import { BurgerMenu } from "../components/lib/BurgerMenu/BurgerMenu";
+import { useAuth } from "../hooks/useAuth";
+import { selectCurrentGameInfo } from "../reducers/CurrentGameSlice";
+import { selectPlayerInfo } from "../reducers/PlayerSlice";
+import { useAppSelector } from "../store";
+import HeaderTMA from "./HeaderTMA";
+import HeaderWeb from "./HeaderWeb";
+import MenuButton from "./MenuButton";
+import { burgerMenuItems } from "../assets/BurgerMenuContent";
+import type { BurgerMenuItemCallable } from "../components/lib/BurgerMenu/BurgerMenuItems";
+
+type HeaderProps = {
+  isMenuOpen: boolean;
+  switchMenuOpen: () => void;
+  closeMenu: () => void;
+};
+
+const Header: React.FC<HeaderProps> = ({ isMenuOpen, switchMenuOpen, closeMenu }) => {
+
+  const { logout } = useAuth();
+
+  const player = useAppSelector(selectPlayerInfo);
+  const currentGame = useAppSelector(selectCurrentGameInfo);
+
+  const TMA = miniApp.ready.isAvailable();
+
+  return (<header className='layout-header'>
+    {TMA ?
+      <HeaderTMA
+        menuButton={<MenuButton isMenuOpen={isMenuOpen} buttonClick={switchMenuOpen} />}
+        title={currentGame?.title}
+        username={player?.username}
+      /> :
+      <HeaderWeb
+        menuButton={<MenuButton isMenuOpen={isMenuOpen} buttonClick={switchMenuOpen} />}
+        title={currentGame?.title}
+        username={player?.username}
+      />
+    }
+
+    {isMenuOpen &&
+      <div className="absolute 
+                        max-sm:right-[5%] max-sm:top-20 max-sm:w-[90%] max-sm:text-lg max-sm:text-center max-sm:rounded-md max-sm:shadow-lg max-sm:border max-sm:border-gray-700
+                        sm:right-14 sm:top-14 sm:w-65 sm:rounded-md sm:shadow-lg sm:border sm:border-gray-700 
+                        focus:ring-blue-200 focus:border-blue-500 bg-gray-800 z-100">
+        <BurgerMenu
+          items={[...burgerMenuItems, { name: `\xa0\xa0\xa0\xa0\xa0\xa0Выйти`, callable: logout } as BurgerMenuItemCallable]}
+          setClose={closeMenu} />
+      </div>}
+  </header>);
+};
+
+export default Header;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,27 +1,10 @@
 import { type ReactNode, useState } from 'react';
-import { BurgerMenu } from '../components/lib/BurgerMenu/BurgerMenu';
-import { useAuth } from '../hooks/useAuth';
 import { NotificationProvider } from '../context/NotificationContext';
 import NotificationPopup from '../components/lib/NotificationPopup';
-import { burgerMenuItems } from '../assets/BurgerMenuContent';
-import { BurgerMenuItemCallable } from '../components/lib/BurgerMenu/BurgerMenuItems';
-import { miniApp } from '@tma.js/sdk-react';
-import HeaderWeb from './HeaderWeb';
-import HeaderTMA from './HeaderTMA';
-import MenuButton from './MenuButton';
-import { useAppSelector } from '../store';
-import { selectPlayerInfo } from '../reducers/PlayerSlice';
-import { selectCurrentGameInfo } from '../reducers/CurrentGameSlice';
+import Header from './Header';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  const { logout } = useAuth();
-
-  const player = useAppSelector(selectPlayerInfo);
-  const currentGame = useAppSelector(selectCurrentGameInfo);
-
   const [isMenuOpen, switchMenuOpen] = useState(false);
-
-  const TMA = miniApp.ready.isAvailable();
 
   const closeBurgerMenu = () => switchMenuOpen(false);
 
@@ -31,30 +14,11 @@ export const Layout = ({ children }: { children: ReactNode }) => {
         <div className="fixed inset-0 bg-black/80 z-40" onClick={closeBurgerMenu} />
       )}
 
-      <header className='layout-header'>
-        {TMA ?
-          <HeaderTMA
-            menuButton={<MenuButton isMenuOpen={isMenuOpen} buttonClick={() => switchMenuOpen(!isMenuOpen)} />}
-            title={currentGame?.title}
-            username={player?.username}
-          /> :
-          <HeaderWeb
-            menuButton={<MenuButton isMenuOpen={isMenuOpen} buttonClick={() => switchMenuOpen(!isMenuOpen)} />}
-            title={currentGame?.title}
-            username={player?.username}
-          />
-        }
-
-        {isMenuOpen &&
-          <div className="absolute 
-                        max-sm:right-[5%] max-sm:top-20 max-sm:w-[90%] max-sm:text-lg max-sm:text-center max-sm:rounded-md max-sm:shadow-lg max-sm:border max-sm:border-gray-700
-                        sm:right-14 sm:top-14 sm:w-65 sm:rounded-md sm:shadow-lg sm:border sm:border-gray-700 
-                        focus:ring-blue-200 focus:border-blue-500 bg-gray-800 z-100">
-            <BurgerMenu
-              items={[...burgerMenuItems, { name: `\xa0\xa0\xa0\xa0\xa0\xa0Выйти`, callable: logout } as BurgerMenuItemCallable]}
-              setClose={closeBurgerMenu} />
-          </div>}
-      </header>
+      <Header 
+        isMenuOpen={isMenuOpen}
+        switchMenuOpen={() => switchMenuOpen(!isMenuOpen)}
+        closeMenu={closeBurgerMenu}
+      />
 
       <main className={`grow p-2 ${isMenuOpen ? 'pointer-events-none select-none' : ''} transition-all duration-200`}>
         <NotificationProvider>
