77de9abb07accb09af33f4b1056780b211926f73
assuntae macbook <pr.polygalova@gmail.com>
2026-04-12 17:58:39 +0700

    refactor: input field classes

--- src/components/lib/Inputs/InputField.tsx
+++ src/components/lib/Inputs/InputField.tsx
@@ -22,13 +22,7 @@ export const InputField: React.FC<InputFieldProps> = ({ label = '', value = "", 
     <div className={`relative ${className}`}>
       <input
         id={id}
-        className={`
-            w-full px-4 py-3 border rounded-lg
-            focus:outline-none focus:ring-2
-            peer
-            ${error ? 'border-red-500 focus:ring-red-200' : 'border-gray-300 focus:ring-blue-200 focus:border-blue-500'}
-            bg-transparent
-          `}
+        className={`input-field peer ${error ? 'input-field-border-error' : 'input-field-border'} `}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         onChange={handleChange}
