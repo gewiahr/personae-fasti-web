dc616ff22ea1db5848f6f8373f5fff2d8b59fbc7
assuntae macbook <pr.polygalova@gmail.com>
2026-04-21 21:20:19 +0700

    fix: notification under header bug

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,27 +9,27 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   const closeBurgerMenu = () => switchMenuOpen(false);
 
   return (<>
-    {isMenuOpen && (
-      <div className="layout-overlay" onClick={closeBurgerMenu} />
-    )}
+    <NotificationProvider>
+      {isMenuOpen && (
+        <div className="layout-overlay" onClick={closeBurgerMenu} />
+      )}
 
-    <div className="layout-main">
-      <Header
-        isMenuOpen={isMenuOpen}
-        switchMenuOpen={() => switchMenuOpen(!isMenuOpen)}
-        closeMenu={closeBurgerMenu}
-      />
+      <div className="layout-main">
+        <Header
+          isMenuOpen={isMenuOpen}
+          switchMenuOpen={() => switchMenuOpen(!isMenuOpen)}
+          closeMenu={closeBurgerMenu}
+        />
 
-      <main className={`grow p-2 ${isMenuOpen ? 'pointer-events-none select-none' : ''} transition-all duration-200`}>
-        <NotificationProvider>
+        <main className={`grow p-2 ${isMenuOpen ? 'pointer-events-none select-none' : ''} transition-all duration-200`}>
           {children}
-          <NotificationPopup />
-        </NotificationProvider>
-      </main>
+        </main>
 
-      <footer className="layout-footer">
-        <a href='https://github.com/gewiahr'>{`© ${new Date().getFullYear()} gewiahr (Tomasz Mozhny)`}</a>
-      </footer>
-    </div>
+        <footer className="layout-footer">
+          <a href='https://github.com/gewiahr'>{`© ${new Date().getFullYear()} gewiahr (Tomasz Mozhny)`}</a>
+        </footer>
+      </div>
+      <NotificationPopup />
+    </NotificationProvider>
   </>);
 };
